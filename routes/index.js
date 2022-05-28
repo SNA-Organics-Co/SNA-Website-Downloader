3e92c82a168c2be0336f2d2551a721f016983e66
TnyavnToツ゚ <aidan61605@gmail.com>
2022-05-28 16:16:06 -0400

    v1

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Website downloader - Take any website offline.' });
+  res.render('index', { title: 'SNA Website downloader' });
 });
 
 module.exports = router;
